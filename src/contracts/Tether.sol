217a74ee926540d00f55d54e0b7b709036ce0556
daviddevops2000 <david.devops2000@gmail.com>
2023-11-24 12:37:19 -0300

    adding functions transactions finantials

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -6,4 +6,32 @@ contract Tether{
     string public symbol = 'mUSDT';
     uint256 public totalSupply = 1000000000000000000;
     uint8 public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+        );
+
+    mapping (address => uint256) public balanceOf;
+
+    constructor(){
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success){
+        // require that the value is greater or equals to transfer
+        require(balanceOf[msg.sender] >= _value);
+        //Transfer  the amount and substract the balances
+        balanceOf[msg.sender]-= _value;
+        // add the balances
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
