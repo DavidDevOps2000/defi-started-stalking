aa619af1365fed6984221f08e27d5a1a64bf220b
daviddevops2000 <david.devops2000@gmail.com>
2023-11-28 17:32:44 -0300

    charging and adding modules DescentralBank

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -1,54 +1,56 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-contract Tether{
-    string public name = 'Tether';
-    string public symbol = 'mUSDT';
+contract Tether {
+    string public name = "Tether";
+    string public symbol = "mUSDT";
     uint256 public totalSupply = 1000000000000000000;
     uint8 public decimals = 18;
 
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint _value
-    );
+    event Transfer(address indexed _from, address indexed _to, uint _value);
     event Approval(
         address indexed _owner,
         address indexed _spender,
         uint _value
-        );
+    );
 
-    mapping (address => uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(){
+    constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint _value) public returns (bool success){
+    function transfer(address _to, uint _value) public returns (bool success) {
         // require that the value is greater or equals to transfer
         require(balanceOf[msg.sender] >= _value);
         //Transfer  the amount and substract the balances
-        balanceOf[msg.sender]-= _value;
+        balanceOf[msg.sender] -= _value;
         // add the balances
         balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success){
-        allowance[msg.sender][_spender] =_value;
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool sucdess){
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool sucdess) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[msg.sender][_from]);
-        balanceOf[_to]+=_value;
-        balanceOf[_from] -=_value;
+        balanceOf[_to] += _value;
+        balanceOf[_from] -= _value;
         allowance[msg.sender][_from] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
-
     }
 }
