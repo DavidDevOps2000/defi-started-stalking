cfe302f65649ea90870cd18b1d004dded0b57e7a
daviddevops2000 <david.devops2000@gmail.com>
2023-11-28 13:22:22 -0300

    adding emit in function approve

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -12,13 +12,14 @@ contract Tether{
         address indexed _to,
         uint _value
     );
-    event Approve(
+    event Approval(
         address indexed _owner,
         address indexed _spender,
         uint _value
         );
 
     mapping (address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(){
         balanceOf[msg.sender] = totalSupply;
@@ -33,5 +34,21 @@ contract Tether{
         balanceOf[_to] += _value;
         emit Transfer(_from, _to, _value);
         return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] =_value;
+        emit Approval(msg.sender, _spender, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool sucdess){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[msg.sender][_from]);
+        balanceOf[_to]+=_value;
+        balanceOf[_from] -=_value;
+        allowance[msg.sender][_from] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+
     }
 }
