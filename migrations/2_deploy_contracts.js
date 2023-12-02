651b782ff3e8100d157d26ec92c479411576dca2
daviddevops2000 <david.devops2000@gmail.com>
2023-12-01 21:00:04 -0300

    adding tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,9 @@
 
+// eslint-disable-next-line no-undef
 const Tether = artifacts.require('Tether');
+// eslint-disable-next-line no-undef
 const RWD = artifacts.require('RWD');
+// eslint-disable-next-line no-undef
 const DecentralBank = artifacts.require('DecentralBank');
 
 module.exports = async function (deployer, network, accounts) {
@@ -16,7 +19,7 @@ module.exports = async function (deployer, network, accounts) {
         await deployer.deploy(DecentralBank, rwd.address, tether.address);
         const decentralBank = await DecentralBank.deployed();
 
-        await rwd.transfer(decentralBank.address,'1000000000000000000000000')
+        await rwd.transfer(decentralBank.address,'100000000000000000000000')
 
         //Distribute 100 Tether tokens to invest
         await tether.transfer(accounts[1],'1000000000000000000')
--- test/decentralBank.tests.js
+++ test/decentralBank.tests.js
@@ -1,23 +1,38 @@
-
+// eslint-disable-next-line no-undef
 const Tether = artifacts.require('Tether.sol');
+// eslint-disable-next-line no-undef
 const RWD = artifacts.require('RWD.sol');
+// eslint-disable-next-line no-undef
 const DecentralBank = artifacts.require('DecentralBank.sol');
 
 require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('decentralBank', (accounts) =>{
+// eslint-disable-next-line no-undef
+contract('decentralBank', ([owner, costumer]) =>{
 
     let tether, rwd, decentralBank
     
+    function tokens(number) {
+        // eslint-disable-next-line no-undef
+        return web3.utils.toWei(number, 'ether')
+        
+    }
     before( async ()=>{
+        //Load Contracts
         tether = await Tether.new()
         rwd = await RWD.new()
         decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+        //Transfer all tokens to DecentralBank (1 Million)
+        await rwd.transfer(decentralBank.address, web3.utils.toWei('1000000'))
+
+        //Trsnfer 100 mock Tether to Costumer
+        await tether.transfer(costumer, tokens('100'), {from: owner})
     })
 //All of code goes here for testing
-describe('Mock Tether Deployment', async ()=>{
+    describe('Mock Tether Deployment', async ()=>{
     it('matches name successfully', async ()=>{
         let tether = await Tether.new();
             assert.equal(name, 'Moch Tether Token');
@@ -25,8 +40,18 @@ describe('Mock Tether Deployment', async ()=>{
 
     describe('Reward Token', async ()=>{
         it('matches name successfully', async ()=>{
-            let name = await rwd.new();
+            let name = await rwd.name();
                 assert.equal(name, 'Reward Token');
+        })
+
+    describe('Decentral Bank Deployment', async ()=>{
+        it('matches name successfully', async ()=>{
+            let name = await decentralBank.name();
+                assert.equal(name, 'Reward Token');
+        })
+        it('contract has token', async ()=>{
+            let balance = await rwd.balanceOf(decentralBank.address)
+            assert.equal(balance, tokens('1000000'))
         })
 })
 })})
