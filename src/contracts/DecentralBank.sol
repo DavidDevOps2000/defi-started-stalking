1f6bb58a13958a9684a0ce690ed603e59d9ce4d4
daviddevops2000 <david.devops2000@gmail.com>
2023-12-03 11:58:16 -0300

    functions unStaking working

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -10,9 +10,62 @@ contract DecentralBank{
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+
     constructor (RWD _rwd, Tether _tether) public{
         rwd = _rwd;
         tether = _tether;
-        
+        owner = msg.sender;
+    }
+
+    function depositTokens(uint _amount) public{
+        require(_amount > 0, 'Amount cannot be 0');
+        //require staking amount to be greater than zero
+
+        // Tranfer tether tokens to this constract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount); 
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+        // Update Staking Balance
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+    function unstakeTokens() public{
+        uint balance = stakingBalance[msg.sender];
+        // Require the amount to be  greater than zero
+
+        require(balance > 0, 'staking balance cannot be less than zero');
+
+        // Transfer the tokens to the specified contract address from our bank
+        tether.transfer(msg.sender, balance);
+
+        // reset staking balance
+        stakingBalance[msg.sender] = 0;
+
+        //Update Staking Status
+        isStaking[msg.sender] = false;
+    }
+
+
+
+    //issue rewards
+    function issueTokens() public{
+        //require the owner to issue tokens only
+        require(msg.sender == owner,'caller must be the owner');
+        for(uint i=0; i > stakers.length; i++){
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient] / 9;// To create  percentege incetive for staking
+            if(balance > 0){
+            rwd.transfer(recipient, balance);
+            }
+        }
+
     }
     }
