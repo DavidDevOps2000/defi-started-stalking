83bd47e41a900a2a8286ce262d40f9406f2deda1
daviddevops2000 <david.devops2000@gmail.com>
2023-12-03 20:56:01 -0300

    adding request to Metamask

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,26 @@ import Navbar from './Navbar';
 import Web3 from 'web3';
 
 class App extends Component{
-    async componentWillUnmount(){
+    async UNSAFE_componentWillMount(){
     await this.loadWeb3()
+    await this.loadBlockchainData()
   }
 
     async loadWeb3(){
-      if(window.ethereuem){
+      if(window.ethereum){
         window.web3 = new Web3(window.ethereum)
-        await window.ethereuem.enable()
+        await window.ethereum.enable()
       }else if (window.web3){
         window.web3 = new Web3(window.web3.currentProvider)
       }else{
         window.alert('No Ethereum browser detected ! You Check out MetaMask')
       }
+    }
+
+    async loadBlockchainData(){
+      const web3 = window.web3
+      const account = await web3.eth.getAccounts()
+      console.log(account)
     }
 
     constructor(props){
